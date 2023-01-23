dc1a9ae88f746b287a65f32f6130081fadc33c7b
ctomrp <cristian.rico.palma@gmail.com>
2023-01-23 02:09:55 -0300

    first commit

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,34 @@
+import todoLogo from "../../assets/todoLogo.svg";
+import { AiOutlinePlusCircle } from "react-icons/ai";
+import styles from "./header.module.css";
+import { useState } from "react";
+
+export function Header({ onAddTask }) {
+  const [title, setTitle] = useState("");
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    onAddTask(title);
+    setTitle("");
+  }
+
+  function onChangeTitle() {
+    setTitle(event.target.value);
+  }
+
+  return (
+    <header className={styles.header}>
+      <img src={todoLogo} />
+
+      <form onSubmit={handleSubmit} className={styles.newTaskForm}>
+        <input
+          placeholder="Add a new task"
+          type="text"
+          value={title}
+          onChange={onChangeTitle}/>
+        <button className={styles.button}>Create<AiOutlinePlusCircle size={20} />
+        </button>
+      </form>
+    </header>
+  );
+}
